fdb2e03bfa51757c9ccfb6682465fc8e67cc7b2a
pagarwal884 <pagarwal1145@gmail.com>
2026-08-27 23:50:23 +0530

    setted up few routes using react router dom and also setted up a little file structure for the base of this project

--- Resume_Builder/src/App.jsx
+++ Resume_Builder/src/App.jsx
@@ -1,12 +1,11 @@
+import { Outlet } from "react-router-dom"
 
 
 const App = () => {
   return (
-    <div>
-      <div className="bg-blue-500 text-white">
-        <h1 className="text-3xl font-bold p-4">Resume Builder</h1>
-      </div>
-    </div>
+    <>
+      <Outlet />
+    </>
   )
 }
 
