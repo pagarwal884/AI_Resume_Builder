dd9a6897d144d288c674879636baad08b7d492f5
pagarwal884 <pagarwal1145@gmail.com>
2026-08-29 14:23:20 +0530

    Fix all th errors and settting up the lin=brary shadcn properly in my repo

--- Resume_Builder/vite.config.js
+++ Resume_Builder/vite.config.js
@@ -1,9 +1,11 @@
 import path from "path"
+import { fileURLToPath } from "url"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
